014618954d549c36608b4f65fee2d940499dc357
Mat Jones <mat@mjones.network>
2024-05-07 14:52:01 -0400

    add delta.nix

---
+++ modules/delta.nix
@@ -0,0 +1,21 @@
+inputs:
+{ config, lib, ... }:
+let
+  inherit (config.tokyonight) style;
+  deltaEnabled = config.programs.git.delta.enable;
+in {
+  options.fish.tokyonight = {
+    enable = lib.mkEnableOption "tokyonight colorscheme";
+  };
+  config = lib.mkMerge [
+    (lib.mkIf deltaEnabled {
+      programs.git = {
+        includes = [{
+          path =
+            "${inputs.tokyonight}/extras/delta/tokyonight_${style}.gitconfig";
+        }];
+        delta.options.features = "tokyonight_${style}";
+      };
+    })
+  ];
+}
