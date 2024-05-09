658be74e19a516a16e2bcaf901bff5069e9e054d
Mat Jones <mat@mjones.network>
2024-05-09 09:39:48 -0400

    update deprecated option

--- modules/bat.nix
+++ modules/bat.nix
@@ -9,8 +9,10 @@ in {
     (lib.mkIf config.programs.bat.tokyonight.enable {
       programs.bat = {
         config.theme = "tokyonight_${style}";
-        themes.${"tokyonight_${style}"} = builtins.readFile
-          "${inputs.tokyonight}/extras/sublime/tokyonight_${style}.tmTheme";
+        themes = {
+          ${"tokyonight_${style}"} = builtins.readFile
+            "${inputs.tokyonight}/extras/sublime/tokyonight_${style}.tmTheme";
+        };
       };
     })
   ];
