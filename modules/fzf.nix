e607ecf70a6d5624542dd4a000fc1ad54a9b193c
Mat Jones <mat@mjones.network>
2024-05-08 20:10:19 -0400

    fix modules

--- modules/fzf.nix
+++ modules/fzf.nix
@@ -6,7 +6,7 @@ in {
     enable = lib.mkEnableOption "tokyonight colorscheme";
   };
   config = lib.mkMerge [
-    (lib.mkIf config.fzf.tokyonight.enable {
+    (lib.mkIf config.programs.fzf.tokyonight.enable {
       programs = {
         # these are .zsh files, but the syntax is compatible
         fish.interactiveShellInit = builtins.readFile
