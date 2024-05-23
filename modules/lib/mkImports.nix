ffbebaf1e4eddc4e8ccef0317ad45ecc5a90a961
Mat Jones <mat@mjones.network>
2024-05-23 09:24:34 -0400

    fix: fix lib

--- modules/lib/mkImports.nix
+++ modules/lib/mkImports.nix
@@ -7,8 +7,7 @@ let
   # in our list, we import them directly
   applyImports = file: _:
     import "${dir}/${file}" (args // {
-      lib =
-        lib.extend (final: _: { ctp = import ./. (args // { lib = final; }); });
+      lib = lib.extend (final: _: (import ./. (args // { lib = final; })));
 
       defaultSources = import ../../.sources;
     });
