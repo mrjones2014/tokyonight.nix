ec8f14b20882170a266ef25cb0ce8f7b37aa1b4a
Mat Jones <mat@mjones.network>
2024-06-11 18:52:11 -0400

    fix(fzf): Fix fzf theme path

--- modules/home-manager/fzf.nix
+++ modules/home-manager/fzf.nix
@@ -7,11 +7,11 @@ in {
       programs = {
         # these are .zsh files, but the syntax is compatible
         fish.interactiveShellInit = builtins.readFile
-          "${inputs.tokyonight}/extras/fzf/tokyonight_${cfg.style}.zsh";
+          "${inputs.tokyonight}/extras/fzf/tokyonight_${cfg.style}.sh";
         bash.initExtra = builtins.readFile
-          "${inputs.tokyonight}/extras/fzf/tokyonight_${cfg.style}.zsh";
+          "${inputs.tokyonight}/extras/fzf/tokyonight_${cfg.style}.sh";
         zsh.initExtra = builtins.readFile
-          "${inputs.tokyonight}/extras/fzf/tokyonight_${cfg.style}.zsh";
+          "${inputs.tokyonight}/extras/fzf/tokyonight_${cfg.style}.sh";
       };
     })
   ];
