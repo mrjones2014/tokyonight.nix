c78ee394333fde97ae7bc5e1f1a38d23d3f32227
Mat Jones <mat@mjones.network>
2024-06-12 11:11:44 -0400

    fix(fzf): Fix paths

--- modules/home-manager/fzf.nix
+++ modules/home-manager/fzf.nix
@@ -1,17 +1,22 @@
 { config, lib, ... }:
-let cfg = config.programs.fzf.tokyonight;
+let
+  cfg = config.programs.fzf.tokyonight;
+  themes = {
+    day = builtins.readFile ../../resources/fzf/tokyonight_day.sh;
+    night = builtins.readFile ../../resources/fzf/tokyonight_night.sh;
+    moon = builtins.readFile ../../resources/fzf/tokyonight_moon.sh;
+    storm = builtins.readFile ../../resources/fzf/tokyonight_storm.sh;
+  };
 in {
   options.programs.fzf.tokyonight = lib.tn.mkTokyonightOpt "tokyonight fzf";
   config = lib.mkMerge [
     (lib.mkIf cfg.enable {
-      programs = {
+      programs = let shell_theme = themes.${cfg.style};
+      in {
         # these are .zsh files, but the syntax is compatible
-        fish.interactiveShellInit =
-          builtins.readFile ../../resources/fzf/tokyonight_${cfg.style}.sh;
-        bash.initExtra =
-          builtins.readFile ../../resources/fzf/tokyonight_${cfg.style}.sh;
-        zsh.initExtra =
-          builtins.readFile ../../resources/fzf/tokyonight_${cfg.style}.sh;
+        fish.interactiveShellInit = shell_theme;
+        bash.initExtra = shell_theme;
+        zsh.initExtra = shell_theme;
       };
     })
   ];
