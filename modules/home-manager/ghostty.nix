59d22b401b235eed0341b7f3263bd8daec429428
Mat Jones <mat@mjones.network>
2025-10-03 22:26:47 -0400

    fix(ghostty): Update theme file name
    fix(ghostty): Update theme file name
    
    fix(ghostty): Update theme file name

--- modules/home-manager/ghostty.nix
+++ modules/home-manager/ghostty.nix
@@ -1,10 +1,16 @@
 { config, lib, ... }:
-let cfg = config.programs.ghostty.tokyonight;
-in {
+let
+  cfg = config.programs.ghostty.tokyonight;
+  toTitleCase =
+    str:
+    lib.strings.toUpper (lib.strings.substring 0 1 str)
+    + lib.strings.substring 1 (lib.strings.stringLength str) str;
+in
+{
   options.programs.ghostty.tokyonight = lib.tn.mkTokyonightOpt "tokyonight ghostty";
   config = lib.mkMerge [
     (lib.mkIf cfg.enable {
-      programs.ghostty.settings.theme = "tokyonight_${cfg.style}";
+      programs.ghostty.settings.theme = "TokyoNight " + (toTitleCase cfg.style);
     })
   ];
 }
