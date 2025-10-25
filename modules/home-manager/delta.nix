85aa747c3a9794fbaa1667a63df7ac743ea99bd6
Mat Jones <mat@mjones.network>
2025-10-25 15:39:04 -0400

    fix(delta): Update options

--- modules/home-manager/delta.nix
+++ modules/home-manager/delta.nix
@@ -1,16 +1,23 @@
-{ inputs, config, lib, ... }:
-let cfg = config.programs.git.delta.tokyonight;
-in {
-  options.programs.git.delta.tokyonight =
-    lib.tn.mkTokyonightOpt "tokyonight git delta";
+{
+  inputs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.programs.git.delta.tokyonight;
+in
+{
+  options.programs.git.delta.tokyonight = lib.tn.mkTokyonightOpt "tokyonight git delta";
   config = lib.mkMerge [
     (lib.mkIf cfg.enable {
+      delta.options.features = "tokyonight_${cfg.style}";
       programs.git = {
-        includes = [{
-          path =
-            "${inputs.tokyonight}/extras/delta/tokyonight_${cfg.style}.gitconfig";
-        }];
-        delta.options.features = "tokyonight_${cfg.style}";
+        includes = [
+          {
+            path = "${inputs.tokyonight}/extras/delta/tokyonight_${cfg.style}.gitconfig";
+          }
+        ];
       };
     })
   ];
